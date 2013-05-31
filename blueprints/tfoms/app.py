58dc855d9d89d32ac9967a2db0c7becaef653fed
SAntipov <santipov@korusconsulting.ru>
2013-05-31 13:41:04 +0400

    определение названия модуля по имени директории

--- blueprints/tfoms/app.py
+++ blueprints/tfoms/app.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
+import os
 from flask import Blueprint
-from .config import MODULE_NAME
 
+# Определяем название модуля по имени директории
+MODULE_NAME = os.path.basename(os.path.dirname(__file__))
 module = Blueprint(MODULE_NAME, __name__, template_folder='templates', static_folder='static')
 
 from views import *
