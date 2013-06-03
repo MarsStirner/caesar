43deebe52347c97c83e9917fd1c437a498990dd2
plakrisenko <PLakrisenko@korusconsulting.ru>
2013-06-03 12:33:35 +0400

    построение дерева тегов, пациенты

--- blueprints/tfoms/utils.py
+++ blueprints/tfoms/utils.py
@@ -1,13 +1,16 @@
 # -*- encoding: utf-8 -*-
+from models import TagsTree
 
-def get_tree(root):
+def get_tree(template_id, root, level, tree):
     """Построение дерева по корню"""
-    level = 0
-    tree = [[root, level]]
-    children = session.query(models.TagsTree.teg_id, models.TagsTree.parent_d).filter_by(parent_id=root[0])
+#    level = 0
+#    tree = [[root, level]]
+    
+    children = TagsTree.query.filter_by(template_id=template_id).filter_by(parent_id=root.tag_id).join(TagsTree.tag).all()
+    #children = session.query(models.TagsTree.teg_id, models.TagsTree.parent_d).filter_by(parent_id=root[0])
     if children:
         level = level + 1
-        for cild in children:
-            tree.append[[child, level]]
-            get_tree(child)
+        for child in children:
+            tree.append([child, level])
+            get_tree(template_id, child, level, tree)
     return tree
