ffbb82c282557d7828dc56b12bf8041fd8f8a6c1
SAntipov <santipov@korusconsulting.ru>
2013-06-28 16:55:17 +0400

    Modified blueprints' path

--- application/app.py
+++ application/app.py
@@ -11,7 +11,7 @@ app.config.from_object(config)
 db.init_app(app)
 
 #Register blueprints
-blueprints_path = os.path.abspath(app.config['BLUEPRINTS_DIR'])
+blueprints_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', app.config['BLUEPRINTS_DIR']))
 load_blueprints(app, apps_path=blueprints_path)
 
 
