c454430edc7bf2647986b62ca4744de696d8ccdc
SAntipov <santipov@korusconsulting.ru>
2013-06-28 14:55:44 +0400

    Some refactoring

--- application/app.py
+++ application/app.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
-from flask import Flask, request, session
-from flask.ext.babelex import Babel
+from flask import Flask
 from database import db
 from autoload import load_blueprints
 import config
@@ -14,19 +13,6 @@ db.init_app(app)
 #Register blueprints
 blueprints_path = os.path.abspath(app.config['BLUEPRINTS_DIR'])
 load_blueprints(app, apps_path=blueprints_path)
-
-
-# Initialize babel
-# TODO: delete if not used
-babel = Babel(app)
-
-
-@babel.localeselector
-def get_locale():
-    override = request.args.get('lang')
-    if override:
-        session['lang'] = override
-    return session.get('lang', 'ru')
 
 
 # Import all views
