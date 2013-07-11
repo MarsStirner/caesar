b794c328ab0bb3e63bfc1039a15c2dc2b85f955b
SAntipov <santipov@korusconsulting.ru>
2013-07-11 22:24:29 +0400

    DB для отчетов

--- blueprints/tfoms/models.py
+++ blueprints/tfoms/models.py
@@ -159,3 +159,112 @@ class ConfigVariables(db.Model):
         return self.code
 
 
+class DownloadFiles(db.Model):
+    __tablename__ = '%s_download_files' % TABLE_PREFIX
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    template_id = db.Column(db.Integer, db.ForeignKey(Template.id, deferrable=True), index=True)
+    name = db.Column(db.String(50), nullable=False)
+    created = db.Column(db.DateTime, default=db.text('NOW()'), nullable=False)
+
+    db.UniqueConstraint(name, created)
+    template = db.relationship(Template)
+
+
+class DownloadBills(db.Model):
+    __tablename__ = '%s_download_bills' % TABLE_PREFIX
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    file_id = db.Column(db.Integer, db.ForeignKey(DownloadFiles.id, deferrable=True))
+    DSCHET = db.Column(db.DateTime, nullable=False)
+    NSCHET = db.Column(db.String(50), nullable=False)
+    CODE_MO = db.Column(db.String(10), nullable=False)
+    YEAR = db.Column(db.Integer, nullable=False)
+    MONTH = db.Column(db.Integer, nullable=False)
+    CODE_MO = db.Column(db.Integer, nullable=False)
+    PLAT = db.Column(db.Integer, nullable=False)
+    SUMMAV = db.Column(db.Numeric(12, 2), nullable=False)
+    start = db.Column(db.Date)
+    end = db.Column(db.Date)
+
+    download_file = db.relationship(DownloadFiles)
+
+
+class DownloadPatients(db.Model):
+    __tablename__ = '%s_download_patients' % TABLE_PREFIX
+
+    id = db.Column(db.Integer, primary_key=True)
+    bill_id = db.Column(db.Integer, db.ForeignKey(DownloadBills.id, deferrable=True), index=True)
+    VPOLIS = db.Column(db.Integer)
+    SPOLIS = db.Column(db.Unicode(20))
+    NPOLIS = db.Column(db.Numeric(30))
+    SMO = db.Column(db.Integer)
+    SMO_OGRN = db.Column(db.Numeric(30))
+    SMO_OK = db.Column(db.Numeric(30))
+    SMO_NAM = db.Column(db.Unicode(100))
+    NOVOR = db.Column(db.Integer, default=0)
+
+    bill = db.relationship(DownloadBills)
+
+
+class DownloadRecords(db.Model):
+    __tablename__ = '%s_download_records' % TABLE_PREFIX
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    bill_id = db.Column(db.Integer, db.ForeignKey(DownloadBills.id, deferrable=True), index=True)
+    patient_id = db.Column(db.Integer, db.ForeignKey(DownloadPatients.id, deferrable=True), index=True)
+    N_ZAP = db.Column(db.Integer)
+    PR_NOV = db.Column(db.Integer, default=0, nullable=False)
+
+
+class DownloadCases(db.Model):
+    __tablename__ = '%s_download_cases' % TABLE_PREFIX
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    record_id = db.Column(db.Integer, db.ForeignKey(DownloadRecords.id, deferrable=True), index=True)
+    actionId = db.Column(db.Integer)
+    eventId = db.Column(db.Integer)
+    rbServiceId = db.Column(db.Integer)
+    USL_OK = db.Column(db.Integer)
+    VIDPOM = db.Column(db.Integer)
+    NPR_MO = db.Column(db.Integer)
+    EXTR = db.Column(db.Integer)
+    LPU = db.Column(db.Integer)
+    LPU_1 = db.Column(db.Integer)
+    PODR = db.Column(db.Integer)
+    PROFIL = db.Column(db.Integer)
+    NHISTORY = db.Column(db.BigInteger)
+    DATE_1 = db.Column(db.Date)
+    DATE_2 = db.Column(db.Date)
+    DS1 = db.Column(db.Unicode(250))
+    CODE_MES1 = db.Column(db.Unicode(250))
+    CODE_MES2 = db.Column(db.Unicode(250))
+    RSLT = db.Column(db.Integer)
+    ISHOD = db.Column(db.Integer)
+    PRVS = db.Column(db.BigInteger)
+    IDDOKT = db.Column(db.BigInteger)
+    IDSP = db.Column(db.Integer)
+    ED_COL = db.Column(db.Numeric(5, 2))
+    SUMV = db.Column(db.Numeric(10, 2))
+
+
+class DownloadServices(db.Model):
+    __tablename__ = '%s_download_services' % TABLE_PREFIX
+
+    id = db.Column(db.Integer, primary_key=True)
+    case_id = db.Column(db.Integer, db.ForeignKey(DownloadCases.id, deferrable=True), index=True)
+    IDSERV = db.Column(db.Integer)
+    LPU = db.Column(db.Integer)
+    LPU_1 = db.Column(db.Integer)
+    PODR = db.Column(db.Integer)
+    PROFIL = db.Column(db.Integer)
+    DATE_IN = db.Column(db.Date)
+    DATE_OUT = db.Column(db.Date)
+    DS = db.Column(db.Unicode(250))
+    CODE_USL = db.Column(db.Unicode(50))
+    KOL_USL = db.Column(db.Numeric(5, 2))
+    TARIF = db.Column(db.Numeric(10, 2))
+    SUMV_USL = db.Column(db.Numeric(10, 2))
+    PRVS = db.Column(db.BigInteger)
+    CODE_MD = db.Column(db.BigInteger)
+
