91add612ca48a04db83a0b451d656dee02a9dd20
SAntipov <santipov@korusconsulting.ru>
2013-07-15 14:36:53 +0400

    Add title to tpls

--- blueprints/tfoms/app.py
+++ blueprints/tfoms/app.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from .config import MODULE_NAME
+from .config import MODULE_NAME, RUS_NAME
 from flask import Blueprint, g
 
 module = Blueprint(MODULE_NAME, __name__, template_folder='templates', static_folder='static')
@@ -16,5 +16,9 @@ def _config(code):
             config[value.code] = value.value
     return config.get(code, None)
 
+
+@module.context_processor
+def module_name():
+    return dict(module_name=RUS_NAME)
 
 from .views import *
