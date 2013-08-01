b4d42b900f0ef1337030e89395db18ed1cb62e06
SAntipov <santipov@korusconsulting.ru>
2013-08-01 18:56:13 +0400

    Authorization

--- application/models.py
+++ application/models.py
@@ -14,3 +14,31 @@ class Settings(db.Model):
 
     def __unicode__(self):
         return self.name
+
+
+class Role(db.Model):
+    __tablename__ = '%s_role' % TABLE_PREFIX
+
+    id = db.Column(db.Integer(), primary_key=True)
+    code = db.Column(db.String(20), unique=True)
+    name = db.Column(db.Unicode(80), unique=True)
+    description = db.Column(db.Unicode(255))
+
+
+class User(db.Model):
+    __tablename__ = '%s_user' % TABLE_PREFIX
+
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean, default=True)
+    roles = db.relationship('Role',
+                            secondary='%s_users_roles' % TABLE_PREFIX,
+                            backref=db.backref('users', lazy='dynamic'))
+
+
+class UsersRoles(db.Model):
+    __tablename__ = '%s_users_roles' % TABLE_PREFIX
+
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id)),
+    role_id = db.Column(db.Integer, db.ForeignKey(Role.id)),
--- application/views.py
+++ application/views.py
@@ -1,12 +1,29 @@
 # -*- encoding: utf-8 -*-
-from flask import render_template, abort, request, redirect, url_for, flash
+from flask import render_template, abort, request, redirect, url_for, flash, session, current_app
 
 from jinja2 import TemplateNotFound
-from flask.ext.wtf import Form, TextField, BooleanField, IntegerField, Required
+from flask.ext.wtf import Form, TextField, PasswordField, IntegerField, Required
+from flask.ext.principal import Principal, Identity, AnonymousIdentity, identity_changed
+from flask.ext.login import LoginManager, login_user, logout_user, login_required, current_user
 
 from application.app import app, db
 from application.context_processors import general_menu
-from models import Settings
+from models import Settings, User
+
+
+Principal(app)
+
+login_manager = LoginManager(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    # Return an instance of the User model
+    return db.session.query(User).get(user_id)
+
+
+class LoginForm(Form):
+    login = TextField()
+    password = PasswordField()
 
 
 @app.route('/')
@@ -40,3 +57,43 @@ def settings():
         return render_template('settings.html', form=form)
     except TemplateNotFound:
         abort(404)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # A hypothetical login form that uses Flask-WTF
+    form = LoginForm()
+
+    # Validate form input
+    if form.validate_on_submit():
+        # Retrieve the user from the hypothetical datastore
+        user = db.session.query(User).filter(login=form.login.data).first()
+
+        # Compare passwords (use password hashing production)
+        if form.password.data == user.password:
+            # Keep the user info in the session using Flask-Login
+            login_user(user)
+
+            # Tell Flask-Principal the identity changed
+            identity_changed.send(current_app._get_current_object(), identity=Identity(user.id))
+
+            return redirect(request.args.get('next') or '/')
+
+    return render_template('user/login.html', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    # Remove the user information from the session
+    logout_user()
+
+    # Remove session keys set by Flask-Principal
+    for key in ('identity.name', 'identity.auth_type'):
+        session.pop(key, None)
+
+    # Tell Flask-Principal the user is anonymous
+    identity_changed.send(current_app._get_current_object(),
+                          identity=AnonymousIdentity())
+
+    return redirect(request.args.get('next') or '/')
