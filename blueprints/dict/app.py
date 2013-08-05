73088999db75768d941bab040558be097c4eaa73
SAntipov <santipov@korusconsulting.ru>
2013-08-05 17:51:47 +0400

    Some refactoring

--- blueprints/dict/app.py
+++ blueprints/dict/app.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
-import os
-import sys
-import pkgutil
 from flask import Blueprint, g
-from werkzeug.utils import import_string
-from application.database import db
-from .models import ConfigVariables
 from .config import MODULE_NAME, RUS_NAME
 
 module = Blueprint(MODULE_NAME, __name__, template_folder='templates', static_folder='static')
@@ -28,14 +22,5 @@ def module_name():
     return dict(module_name=RUS_NAME)
 
 
-path = os.path.join(os.path.dirname(__file__), 'views')
-modules = pkgutil.iter_modules(path=[path])
-
-for loader, mod_name, ispkg in modules:
-    # Ensure that module isn't already loaded
-    if mod_name not in sys.modules:
-        # Import module
-        import_string(path+"."+mod_name)
-
-# from .views import *
-# from .views.tariff import *
+from .views import *
+from .views.tariff import *
