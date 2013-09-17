47d8f6b1e6a14805ff9afad51ceca0414fac37b6
SAntipov <santipov@korusconsulting.ru>
2013-09-17 17:36:38 +0400

    Config fix

--- blueprints/reports/utils.py
+++ blueprints/reports/utils.py
@@ -13,12 +13,11 @@ _config = create_config_func(MODULE_NAME, ConfigVariables)
 def get_lpu_session():
     Session = None
     try:
-        LPU_DB_CONNECT_STRING = LPU_DB_CONNECT_STRING
-    except Exception, e:
-        print e
-    else:
         engine = create_engine(LPU_DB_CONNECT_STRING, convert_unicode=True, pool_recycle=600)
         Session = scoped_session(sessionmaker(bind=engine))
+    except Exception, e:
+        print e
+
     if Session is None:
         raise AttributeError(u'Не настроено подключение к БД ЛПУ')
     return Session()
