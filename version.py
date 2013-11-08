8554ad8b7404f94e853c23d9993ccb238782c873
SAntipov <santipov@korusconsulting.ru>
2013-11-08 17:05:49 +0400

    WMIS-59: striptags in <NHISTORY>

--- version.py
+++ version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-version = u'1.2'
-last_change_date = datetime(year=2013, month=11, day=7, hour=20,)
+version = u'1.2.1'
+last_change_date = datetime(year=2013, month=11, day=8, hour=17,)
