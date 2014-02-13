500ab5bbf7ec0373902255421c4b9d533d1fc053
plakrisenko <PLakrisenko@korusconsulting.ru>
2014-02-13 12:03:18 +0400

    подсистема печати

--- blueprints/print_subsystem/lib/data.py
+++ blueprints/print_subsystem/lib/data.py
@@ -24,7 +24,7 @@ class Print_Template(object):
 
     def print_template(self, template_id, event_id, action_id, client_id, additional_context):
         # context = CInfoContext()
-        client = self.db_session.query(Client).get(18)
+        client = self.db_session.query(Client).get(client_id)
         event = self.db_session.query(Event).get(event_id)
         action = self.db_session.query(Action).get(action_id)
         quota = self.db_session.query(v_Client_Quoting).filter_by(event_id=event_id).filter_by(clientId=client_id).first()
--- blueprints/print_subsystem/views.py
+++ blueprints/print_subsystem/views.py
@@ -39,14 +39,14 @@ def template_meta():
 @public_endpoint
 @module.route('/print_template', methods=["POST", "GET"])
 def print_template():
-    # try:
-    data = json.loads(request.data)
-    template_id = data['id']
-    action_id = data['action_id']
-    event_id = data['event_id']
-    client_id = data['client_id']
-    additional_context = data['additional_context']
-    print_obj = Print_Template()
-    return print_obj.print_template(template_id, event_id, action_id, client_id, additional_context)
-    # except TemplateNotFound:
-    #     abort(404)
+    try:
+        data = json.loads(request.data)
+        template_id = data['id']
+        action_id = data['action_id']
+        event_id = data['event_id']
+        client_id = data['client_id']
+        additional_context = data['additional_context']
+        print_obj = Print_Template()
+        return print_obj.print_template(template_id, event_id, action_id, client_id, additional_context)
+    except TemplateNotFound:
+        abort(404)
