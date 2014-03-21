2918ec417da40b29fd3b4985c8163a8cae5e5ecc
swod <roman.s3000@gmail.com>
2014-03-21 20:45:54 +0400

    Бутстрап 3 для главной страницы модуля ТФОМС

--- blueprints/tfoms/app.py
+++ blueprints/tfoms/app.py
@@ -10,4 +10,30 @@ module = Blueprint(MODULE_NAME, __name__, template_folder='templates', static_fo
 def module_name():
     return dict(module_name=RUS_NAME)
 
+
+@module.context_processor
+def menu_struct():
+    m = [{'name': u'Главная',
+          'url': '.index',
+          },
+         {'name': u'Выгрузка',
+          'url': '.download',
+          },
+         {'name': u'Загрузка',
+          'url': '.upload',
+          },
+         {'name': u'Отчеты',
+          'url': '.reports',
+          },
+         {'name': u'Настройки',
+          'subitems': [{'name': u'Шаблоны',
+                        'url': '.add_new_template'},
+                       {'name': u'Настройки модуля',
+                        'url': '.settings'}],
+          'restrict_access': True,
+         }
+         ]
+    return dict(menu_struct=m)
+
+
 from .views import *
