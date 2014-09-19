2ad95ff52b67e846431193b4d4cb8d152c1d9c61
plakrisenko <PLakrisenko@korusconsulting.ru>
2014-09-19 12:23:05 +0400

    fix, печать документов в форме обращения

--- blueprints/print_subsystem/models/models_all.py
+++ blueprints/print_subsystem/models/models_all.py
@@ -1791,7 +1791,8 @@ class Clientdocument(db.Model, Info):
         return self.documentType.regionalCode
 
     def __unicode__(self):
-        return (' '.join([self.documentType.name if self.documentType else '', self.serial, self.number])).strip()
+        return (' '.join([self.documentType.name if self.documentType else '', self.serial if self.serial else '',
+                          self.number if self.number else ''])).strip()
 
 
 class Clientfdproperty(db.Model):
