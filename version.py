04c99edbbeffc917099620728fc59d7042f638d5
SAntipov <santipov@korusconsulting.ru>
2014-10-31 14:39:20 +0300

    v1.4.5

--- version.py
+++ version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-version = u'1.4.4'
-last_change_date = datetime(year=2014, month=10, day=29, hour=20,)
+version = u'1.4.5'
+last_change_date = datetime(year=2014, month=10, day=31, hour=14,)
