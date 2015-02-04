656ff9162849c9fa27781a7742c7701c35308b3e
viruzzz-kun <viruzzz.soft@gmail.com>
2015-02-04 18:55:16 +0300

    RIMIS-154 Набросок настроек РИСАР

---
+++ blueprints/risar_config/static/js/app.js
@@ -0,0 +1,11 @@
+/**
+ * Created by mmalkov on 04.02.15.
+ */
+
+RisarSetupApp = angular.module('RisarSetupApp', []);
+
+RisarSetupApp.controller('SetupMainCtrl', function ($scope, $window) {
+    $scope.page = 0;
+    $scope.save = function () {
+    }
+});
---
+++ blueprints/risar_config/views/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+import html
+
+__author__ = 'viruzzz-kun'
