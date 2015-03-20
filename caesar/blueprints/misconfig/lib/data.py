6556dab49f9c85016cc7a02b89ba15da762b47ff
SAntipov <santipov@korusconsulting.ru>
2015-03-20 17:04:15 +0300

    QuotaType API: GET/POST/DELETE

--- caesar/blueprints/misconfig/lib/data.py
+++ caesar/blueprints/misconfig/lib/data.py
@@ -1,30 +1,66 @@
 # -*- coding: utf-8 -*-
 from dateutil import parser
 from nemesis.systemwide import db
-from nemesis.models.exists import QuotaCatalog
+from nemesis.models.exists import QuotaCatalog, QuotaType
 
 
 def worker(model):
     if model == QuotaCatalog:
         return QuotaCatalogWorker(model)
+    if model == QuotaType:
+        return QuotaTypeWorker(model)
 
 
-class QuotaCatalogWorker(object):
+class BaseWorker(object):
 
     def __init__(self, model):
         self.model = model
 
-    def get_list(self, order=None):
-        return self.model.query.order_by(order).all()
-
     def get_by_id(self, _id):
         return self.model.query.get(_id)
 
-    def __fill_obj(self, obj, data):
+    def get_list(self, where=None, order=None):
+        query = self.model.query
+        if where is not None:
+            query = query.filter(where)
+        if order is not None:
+            query = query.order_by(order)
+        return query.all()
+
+    def _fill_obj(self, obj, data):
+        raise NotImplementedError('Please implement this method')
+
+    def add(self, data):
+        obj = self._fill_obj(self.model(), data)
+        db.session.add(obj)
+        db.session.commit()
+        return obj
+
+    def update(self, _id, data):
+        obj = self.get_by_id(_id)
+        if not obj:
+            return None
+        obj = self._fill_obj(obj, data)
+        db.session.add(obj)
+        db.session.commit()
+        return obj
+
+    def delete(self, _id):
+        obj = self.get_by_id(_id)
+        if not obj:
+            return None
+        db.session.delete(obj)
+        db.session.commit()
+        return True
+
+
+class QuotaCatalogWorker(BaseWorker):
+
+    def _fill_obj(self, obj, data):
         if 'finance_id' in data:
             obj.finance_id = data['finance_id']
-        if 'create_datetime' in data:
-            obj.createDatetime = data['create_datetime']
+        if 'create_datetime' in data and data['create_datetime']:
+            obj.createDatetime = parser.parse(data['create_datetime'])
         if 'create_person_id' in data:
             obj.createPerson_id = data['create_person_id']
         if 'beg_date' in data and data['beg_date']:
@@ -39,25 +75,30 @@ class QuotaCatalogWorker(object):
             obj.comment = data['comment']
         return obj
 
-    def add(self, data):
-        obj = self.__fill_obj(self.model(), data)
-        db.session.add(obj)
-        db.session.commit()
-        return obj
 
-    def update(self, _id, data):
-        obj = self.get_by_id(_id)
-        if not obj:
-            return None
-        obj = self.__fill_obj(obj, data)
-        db.session.add(obj)
-        db.session.commit()
-        return obj
+class QuotaTypeWorker(BaseWorker):
 
-    def delete(self, _id):
-        obj = self.get_by_id(_id)
-        if not obj:
-            return None
-        db.session.delete(obj)
-        db.session.commit()
-        return True
+    def _fill_obj(self, obj, data):
+        if 'catalog_id' in data:
+            obj.catalog_id = data['catalog_id']
+        if 'create_datetime' in data and data['create_datetime']:
+            obj.createDatetime = parser.parse(data['create_datetime'])
+        if 'create_person_id' in data:
+            obj.createPerson_id = data['create_person_id']
+        if 'deleted' in data:
+            obj.deleted = data['deleted']
+        if 'class' in data:
+            obj.class_ = data['class']
+        if 'profile_code' in data:
+            obj.profile_code = data['profile_code']
+        if 'group_code' in data:
+            obj.group_code = data['group_code']
+        if 'type_code' in data:
+            obj.type_code = data['type_code']
+        if 'code' in data:
+            obj.code = data['code']
+        if 'name' in data:
+            obj.name = data['name']
+        if 'price' in data:
+            obj.price = float(data['price'])
+        return obj
--- caesar/blueprints/misconfig/views/api.py
+++ caesar/blueprints/misconfig/views/api.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from flask import request
 
+from nemesis.systemwide import db
 from nemesis.lib.apiutils import api_method, ApiException
-from nemesis.models.exists import QuotaCatalog
+from nemesis.models.exists import QuotaCatalog, QuotaType
 
 from ..app import module
 from ..lib.data import worker
@@ -44,3 +45,45 @@ def api_v1_quota_catalog_delete(_id):
     if result is None:
         raise ApiException(404, u'Значение с id={0} не найдено'.format(_id))
     return result
+
+
+@module.route('/api/v1/quota_type/<int:catalog_id>', methods=['GET'])
+@module.route('/api/v1/quota_type/<int:catalog_id>/<int:_id>', methods=['GET'])
+@api_method
+def api_v1_quota_type_get(catalog_id, _id=None):
+    obj = worker(QuotaType)
+    if _id is not None:
+        data = obj.get_by_id(_id)
+        if not data:
+            raise ApiException(404, u'Значение с id={0} не найдено'.format(_id))
+        return data
+    return obj.get_list(where=db.and_(QuotaType.catalog_id == catalog_id, QuotaType.deleted == 0),
+                        order=QuotaType.group_code)
+
+
+@module.route('/api/v1/quota_type/<int:catalog_id>', methods=['POST'])
+@module.route('/api/v1/quota_type/<int:catalog_id>/<int:_id>', methods=['POST'])
+@api_method
+def api_v1_quota_type_post(catalog_id, _id=None):
+    obj = worker(QuotaType)
+    data = request.get_json()
+    if 'catalog_id' not in data:
+        data.update({'catalog_id': catalog_id})
+    if _id is not None:
+        result = obj.update(_id, data)
+        if result is None:
+            raise ApiException(404, u'Значение с id={0} не найдено'.format(_id))
+    else:
+        result = obj.add(data)
+    return result
+
+
+@module.route('/api/v1/quota_type/<int:catalog_id>/<int:_id>', methods=['DELETE'])
+@api_method
+def api_v1_quota_type_delete(catalog_id, _id):
+    obj = worker(QuotaType)
+    result = obj.delete(_id)
+    if result is None:
+        raise ApiException(404, u'Значение с id={0} не найдено'.format(_id))
+    return result
+
