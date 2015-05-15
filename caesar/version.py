39d89cbff727634631fe4882ce20b073bbf951ea
swod <roman.s3000@gmail.com>
2015-05-15 17:22:28 +0300

    Version 1.5.3

--- caesar/version.py
+++ caesar/version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-version = u'1.5.2'
-last_change_date = datetime(year=2015, month=5, day=12, hour=14)
+version = u'1.5.3'
+last_change_date = datetime(year=2015, month=5, day=15, hour=17)
