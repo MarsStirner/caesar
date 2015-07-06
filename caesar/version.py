4d9607f4e5853d72188d15e95205682083bdf791
SAntipov <santipov@korusconsulting.ru>
2015-07-06 15:35:41 +0300

    v1.6dev3

--- caesar/version.py
+++ caesar/version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-version = u'1.6dev2'
-last_change_date = datetime(year=2015, month=6, day=11, hour=14)
+version = u'1.6dev3'
+last_change_date = datetime(year=2015, month=7, day=6, hour=15)
