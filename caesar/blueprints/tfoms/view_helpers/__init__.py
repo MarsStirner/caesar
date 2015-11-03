6a91406bd7ecea24e4870e6a2cd8afe467f10488
swod <roman.s3000@gmail.com>
2015-11-03 14:11:42 +0300

    Убрана проверка на наличие настроек для тфомс

--- caesar/blueprints/tfoms/view_helpers/__init__.py
+++ caesar/blueprints/tfoms/view_helpers/__init__.py
@@ -8,4 +8,5 @@ with app.app_context():
     elif _config('region_code') == 'spb':
         from .spb import *
     else:
-        raise ImportError(u'Cannot import views for {0}'.format(_config('region_code')))
+        pass
+        # raise ImportError(u'Cannot import views for {0}'.format(_config('region_code')))
