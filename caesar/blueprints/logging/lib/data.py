72a64ad94b86ab71e78d950d3c44e1391a815805
Михаил Мальков <m.malkov@bars-open.ru>
2016-03-18 12:05:31 +0300

    Ремонт доступа к Simplelogs

--- caesar/blueprints/logging/lib/data.py
+++ caesar/blueprints/logging/lib/data.py
@@ -1,11 +1,11 @@
 # -*- encoding: utf8 -*-
+from nemesis.app import app
 from pysimplelogs2 import Simplelog
-from config import SIMPLELOGS_URL
 
 
 class Log_Data(object):
     def __init__(self):
-        self.simplelogs = Simplelog(SIMPLELOGS_URL)
+        self.simplelogs = Simplelog(app.config['SIMPLELOGS_URL'])
 
     def get_owners(self):
         return self.simplelogs.get_owners()
