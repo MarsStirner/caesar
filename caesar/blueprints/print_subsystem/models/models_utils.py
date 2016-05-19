d5306333dd8a4204a8c7fe367b89fae400fee87e
Михаил Мальков <m.malkov@bars-open.ru>
2016-05-19 16:25:57 +0300

    TMIS-1104 Fix 4

--- caesar/blueprints/print_subsystem/models/models_utils.py
+++ caesar/blueprints/print_subsystem/models/models_utils.py
@@ -52,7 +52,8 @@ def code128C_any(string):
         if number == 0:
             break
     result.reverse()
-    result.append(reduce(lambda x, (c, y): (x + y * (c + 1)) % 103, enumerate(result), 105))
+    result.append(sum(map(lambda (k, c): (k+1)*c, enumerate(result + [105]))) % 103)
+    # result.append(reduce(lambda x, (c, y): (x + y * (c + 1)) % 103, enumerate(result), 105))
     result = [0xcd] + [
         (w + 100 if w > 94 else w + 32)
         for w in result
