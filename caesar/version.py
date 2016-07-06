825d561044d29af0739bed59332cf3c579c7d543
Aleksandr Krivopustov <a.krivopustov@bars-open.ru>
2016-07-06 20:25:26 +0300

    RISAR Version 1.8.5

--- caesar/version.py
+++ caesar/version.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# # -*- coding: utf-8 -*-
 from datetime import datetime
-version = u'rsr1.8.4'
-last_change_date = datetime(year=2016, month=6, day=30, hour=17)
+version = u'rsr1.8.5'
+last_change_date = datetime(year=2016, month=7, day=6, hour=20)
