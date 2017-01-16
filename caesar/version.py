2f1def833a2352f273cc9aadc32eec2d1d4ed0f5
Роман Степанов <r.stepanov@bars-open.ru>
2017-01-16 15:11:46 +0300

    RISAR Version 1.9.16

--- caesar/version.py
+++ caesar/version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-version = u'rsr1.9.15'
-last_change_date = datetime(year=2016, month=12, day=19, hour=21)
+version = u'rsr1.9.16'
+last_change_date = datetime(year=2017, month=1, day=16, hour=15)
