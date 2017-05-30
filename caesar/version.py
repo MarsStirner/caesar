735473a4edeaac0e80fb4df712704dc1cf03813f
Oleg Tkachenko <o.tkachenko@bars-open.ru>
2017-05-30 12:31:11 +0200

    Version 1.8.13

--- caesar/version.py
+++ caesar/version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-version = u'1.8.12'
-last_change_date = datetime(year=2017, month=4, day=24, hour=15)
+version = u'1.8.13'
+last_change_date = datetime(year=2017, month=5, day=30, hour=12)
