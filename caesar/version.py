8e4672382434a4bf2473a8f63ff80e05ff933e15
Роман Степанов <r.stepanov@bars-open.ru>
2017-06-15 18:12:48 +0300

    Version 1.8.14

--- caesar/version.py
+++ caesar/version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-version = u'1.8.13'
-last_change_date = datetime(year=2017, month=5, day=30, hour=12)
+version = u'1.8.14'
+last_change_date = datetime(year=2017, month=6, day=15, hour=18)
